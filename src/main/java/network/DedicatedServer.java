be2ab59db7f50d2afa95a46680bfffdae658e8b7
FerranMontoliu <ferran.montoliu@students.salle.url.edu>
2019-04-03 10:49:58 +0200

    Ara peta per tot arreu segurament però està a mitges

--- src/main/java/network/DedicatedServer.java
+++ src/main/java/network/DedicatedServer.java
@@ -2,18 +2,26 @@ package network;
 
 import model.User;
 
-import java.io.DataInputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class DedicatedServer extends Thread {
 
+    private static final char LOGIN_USER = 'a';
+    private static final char REGISTER_USER = 'b';
+    private static final char EDIT_PROFILE = 'c';
+    private static final char USER_MATCHED = 'd';
+    private static final char USER_UNMATCHED = 'e';
+    private static final char LOAD_CHAT = 'f';
+    private static final char SEND_MESSAGE = 'g';
+
     private boolean isOn;
     private Socket sClient;
     private DataInputStream dataInput;
+    private DataOutputStream dataOutput;
+    private ObjectInputStream objectIn;
     private ObjectOutputStream objectOut;
     private LinkedList<DedicatedServer> clients;
     private ArrayList<User> model;
@@ -59,13 +67,56 @@ public class DedicatedServer extends Thread {
     public void run() {
         try {
             dataInput = new DataInputStream(sClient.getInputStream());
+            dataOutput = new DataOutputStream(sClient.getOutputStream());
+            objectIn = new ObjectInputStream(sClient.getInputStream());
             objectOut = new ObjectOutputStream(sClient.getOutputStream());
 
             objectOut.writeObject(new UserState(model));
             while(isOn) {
-                //Rebre dades i tractar-les.
 
-                updateAllClients();
+                //Llegir char que indica quin missatge rebrà el servidor:
+                char func = dataInput.readChar();
+                switch(func){
+                    case LOGIN_USER:
+                        //TODO: llegir user.
+                        //TODO: comprovar a la base de dades si l'usuari existeix. En cas afirmatiu retornar true, altrament retornar false.
+                        boolean userExistsL = false;
+                        dataOutput.writeBoolean(userExistsL);
+                        break;
+
+                    case REGISTER_USER:
+                        //TODO: llegir user.
+                        //TODO: comprovar a la base de dades si l'usuari existeix. En cas afirmatiu retornar true, altrament retornar false.
+                        boolean userExistsR = false;
+                        dataOutput.writeBoolean(userExistsR);
+                        break;
+
+                    case EDIT_PROFILE:
+                        //TODO: llegir user.
+                        //TODO: escriure els nous paràmetres a la base de dades.
+                        break;
+
+                    case USER_MATCHED:
+                        //TODO: llegir 2 usuaris que han fet match.
+                        //TODO: connectar amb la base de dades i ficar cada usuari a la llista de match de l'altre.
+                        break;
+
+                    case USER_UNMATCHED:
+                        //TODO: llegir 2 usuaris que han fet match.
+                        //TODO: connectar amb la base de dades i treure cada usuari de la llista de match de l'altre.
+                        break;
+
+                    case LOAD_CHAT:
+                        //TODO: llegir 2 usuaris que conformen el xat.
+                        //TODO: connectar amb la base de dades i agafar el seu xat.
+                        break;
+
+                    case SEND_MESSAGE:
+                        //TODO: llegir String.
+                        //TODO: connectar amb la base de dades i afegir el String a la llista de missatges que conforma el xat.
+                        break;
+                }
+                //updateAllClients();
             }
         } catch (IOException e1) {
             stopDedicatedServer();
