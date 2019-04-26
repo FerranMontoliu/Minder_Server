2e424a7bef57b552983136ee7275aeb3855a1a07
Alba Massa <albamassasentis@gmail.com>
2019-04-26 23:03:39 +0200

    view updated (included age filter)

--- src/main/java/view/JRegister.java
+++ src/main/java/view/JRegister.java
@@ -19,13 +19,14 @@ public class JRegister extends JPanel {
     private JRadioButton jrbNoPremium;
     private JButton jbRegister;
     private JCheckBox jcbViewPassword;
+    private JComboBox jcbMinAgeFilter;
+    private JComboBox jcbMaxAgeFilter;
     private char ecoChar;
 
 
 
     public JRegister(){
-        setLayout(new GridLayout(17,1));
-
+        setLayout(new GridLayout(21,1));
         //Inicialitzar els elements
         jtfAge = new JTextField();
         jtfName = new JTextField();
@@ -38,6 +39,8 @@ public class JRegister extends JPanel {
         jrbNoPremium = new JRadioButton("No");
         jbRegister = new JButton("Register");
         jcbViewPassword = new JCheckBox();
+
+
 
         //Grup de radioButtons
         ButtonGroup bg = new ButtonGroup();
@@ -72,16 +75,99 @@ public class JRegister extends JPanel {
         jpRadioButton.add(jrbPremium);
         add (new JLabel("Premium"));
         add (jpRadioButton);
-        add(jbRegister);
+
+        createAgeFilters();
+        //add (new JLabel("Minmum age filter"));
+        //add (jcbMinAgeFilter);
+        //add (new JLabel("Maximum age filter"));
+        //add (jcbMaxAgeFilter);
+        add (jbRegister);
 
 
     }
 
     public User getUser() {
         //TODO: update age filter parameters (last two nulls)
-        User u = new User(jtfName.getText(), jtfAge.getText(), jrbPremium.isSelected(), jtfMail.getText(), new String (jpfPassword.getPassword()),null, null);
+        User u = new User(jtfName.getText(), jtfAge.getText(), jrbPremium.isSelected(), jtfMail.getText(), new String (jpfPassword.getPassword()),jcbMinAgeFilter.getSelectedItem().toString(), jcbMaxAgeFilter.getSelectedItem().toString());
 
         return u;
+    }
+
+    private void createAgeFilters() {
+        JPanel jpFilterAge = new JPanel();
+        jpFilterAge.setLayout(new FlowLayout());
+
+        JPanel jpBothFilters = new JPanel();
+        jpBothFilters.setLayout(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+
+        //Minimum Age
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.weightx = 1.0f;
+        constraints.weighty = 1.0f;
+        constraints.insets = new Insets(5, 5, 5, 5);
+        constraints.fill = GridBagConstraints.WEST;
+
+        JLabel jlMin = new JLabel("Minimum age:");
+        jlMin.setHorizontalAlignment(SwingConstants.CENTER);
+        jpBothFilters.add(jlMin, constraints);
+
+        //Min age comboBox
+        jcbMinAgeFilter = new JComboBox<>();
+        jcbMinAgeFilter.setEditable(false);
+        jcbMinAgeFilter.setPreferredSize(new Dimension(100,30));
+        jcbMinAgeFilter.setMaximumSize(jcbMinAgeFilter.getPreferredSize());
+
+        for (int i = 18; i <= 100; ++i) {
+            jcbMinAgeFilter.addItem(i);
+        }
+
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.weightx = 1.0f;
+        constraints.weighty = 1.0f;
+        constraints.insets = new Insets(5, 2, 5, 5);
+        constraints.fill = GridBagConstraints.CENTER;
+
+        jpBothFilters.add(jcbMinAgeFilter, constraints);
+
+        //Maximum Age
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        constraints.weightx = 1.0f;
+        constraints.weighty = 1.0f;
+        constraints.insets = new Insets(5, 5, 5, 5);
+        constraints.fill = GridBagConstraints.WEST;
+
+        JLabel jlMax = new JLabel("Maximum age:");
+        jlMax.setHorizontalAlignment(SwingConstants.CENTER);
+        jpBothFilters.add(jlMax, constraints);
+
+        //Max age comboBox
+
+        jcbMaxAgeFilter = new JComboBox<>();
+        jcbMaxAgeFilter.setEditable(false);
+        jcbMaxAgeFilter.setPreferredSize(new Dimension(100,30));
+        jcbMaxAgeFilter.setMaximumSize(jcbMaxAgeFilter.getPreferredSize());
+
+        for (int i = 18; i <= 100; ++i) {
+            jcbMaxAgeFilter.addItem(i);
+        }
+
+        constraints.gridx = 1;
+        constraints.gridy = 2;
+        constraints.weightx = 1.0f;
+        constraints.weighty = 1.0f;
+        constraints.insets = new Insets(5, 5, 5, 2);
+        constraints.fill = GridBagConstraints.CENTER;
+
+        jpBothFilters.add(jcbMaxAgeFilter, constraints);
+
+        jpFilterAge.add(jpBothFilters);
+
+        add(jpFilterAge);
+        //add(Box.createVerticalStrut(10));
     }
 
     public void removeRegister(){
